ce25694b126eebbb59ed1b7a22bdc6064c349437
Eben Freeman <eben@honeycomb.io>
2018-01-02 14:20:52 -0800

    Fix test build error

--- ast_test.go
+++ ast_test.go
@@ -13,66 +13,66 @@ func TestLimits(t *testing.T) {
 		t.Errorf("got %v, %v, %v, want nils", o, r, err)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("aa"))}
+	l = &Limit{Offset: NumVal([]rune("aa"))}
 	_, _, err = l.Limits()
 	wantErr := "strconv.ParseInt: parsing \"aa\": invalid syntax"
 	if err == nil || err.Error() != wantErr {
 		t.Errorf("got %v, want %s", err, wantErr)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("2"))}
+	l = &Limit{Offset: NumVal([]rune("2"))}
 	_, _, err = l.Limits()
 	wantErr = "unexpected node for rowcount: <nil>"
 	if err == nil || err.Error() != wantErr {
 		t.Errorf("got %v, want %s", err, wantErr)
 	}
 
-	l = &Limit{Offset: StrVal([]byte("2"))}
+	l = &Limit{Offset: StrVal([]rune("2"))}
 	_, _, err = l.Limits()
 	wantErr = "unexpected node for offset: [50]"
 	if err == nil || err.Error() != wantErr {
 		t.Errorf("got %v, want %s", err, wantErr)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("2")), Rowcount: NumVal([]byte("aa"))}
+	l = &Limit{Offset: NumVal([]rune("2")), Rowcount: NumVal([]rune("aa"))}
 	_, _, err = l.Limits()
 	wantErr = "strconv.ParseInt: parsing \"aa\": invalid syntax"
 	if err == nil || err.Error() != wantErr {
 		t.Errorf("got %v, want %s", err, wantErr)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("2")), Rowcount: NumVal([]byte("3"))}
+	l = &Limit{Offset: NumVal([]rune("2")), Rowcount: NumVal([]rune("3"))}
 	o, r, err = l.Limits()
 	if o.(int64) != 2 || r.(int64) != 3 || err != nil {
 		t.Errorf("got %v %v %v, want 2, 3, nil", o, r, err)
 	}
 
-	l = &Limit{Offset: ValArg([]byte(":a")), Rowcount: NumVal([]byte("3"))}
+	l = &Limit{Offset: ValArg([]rune(":a")), Rowcount: NumVal([]rune("3"))}
 	o, r, err = l.Limits()
 	if o.(string) != ":a" || r.(int64) != 3 || err != nil {
 		t.Errorf("got %v %v %v, want :a, 3, nil", o, r, err)
 	}
 
-	l = &Limit{Offset: nil, Rowcount: NumVal([]byte("3"))}
+	l = &Limit{Offset: nil, Rowcount: NumVal([]rune("3"))}
 	o, r, err = l.Limits()
 	if o != nil || r.(int64) != 3 || err != nil {
 		t.Errorf("got %v %v %v, want nil, 3, nil", o, r, err)
 	}
 
-	l = &Limit{Offset: nil, Rowcount: ValArg([]byte(":a"))}
+	l = &Limit{Offset: nil, Rowcount: ValArg([]rune(":a"))}
 	o, r, err = l.Limits()
 	if o != nil || r.(string) != ":a" || err != nil {
 		t.Errorf("got %v %v %v, want nil, :a, nil", o, r, err)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("-2")), Rowcount: NumVal([]byte("0"))}
+	l = &Limit{Offset: NumVal([]rune("-2")), Rowcount: NumVal([]rune("0"))}
 	_, _, err = l.Limits()
 	wantErr = "negative offset: -2"
 	if err == nil || err.Error() != wantErr {
 		t.Errorf("got %v, want %s", err, wantErr)
 	}
 
-	l = &Limit{Offset: NumVal([]byte("2")), Rowcount: NumVal([]byte("-2"))}
+	l = &Limit{Offset: NumVal([]rune("2")), Rowcount: NumVal([]rune("-2"))}
 	_, _, err = l.Limits()
 	wantErr = "negative limit: -2"
 	if err == nil || err.Error() != wantErr {
